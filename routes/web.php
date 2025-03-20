1a5a1787e162f2f20049b4557371305e511fbaa7
Baba-chicharitos <baptiste.ser09@gmail.com>
2025-03-20 17:47:58 +0100

    test 4

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,46 @@
 
 use Illuminate\Support\Facades\Route;
 
+/////////////////////////////////////////////
+//                                         //
+//                                         //
+//       Route vers la page de garde       //
+//                                         //
+//                                         //
+/////////////////////////////////////////////
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/comptable_validation', function () {
-    return view('comptable_validation');
+
+/////////////////////////////////////////////
+//                                         //
+//                                         //
+//    Route vers les pages des clients     //
+//                                         //
+//                                         //
+/////////////////////////////////////////////
+Route::get('/cliChoix', function () {
+    return view('cliChoix');
 });
 
+
+Route::get('/cliConsulter', function () {
+    return view('cliConsulter');
+});
+
+Route::get('/cliSaisir', function () {
+    return view('cliSaisir');
+});
+
+/////////////////////////////////////////////
+//                                         //
+//                                         //
+//   Route vers les pages des comptables   //
+//                                         //
+//                                         //
+/////////////////////////////////////////////
+
+Route::get('/comValidation', function () {
+    return view('comValidation');
+});
